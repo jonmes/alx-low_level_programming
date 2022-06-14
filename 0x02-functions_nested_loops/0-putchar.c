8e4e98ff4581ab2ecd18fc71609cf82bb81f4895
jonmes <mesfinjohn@rocketmail.com>
2022-06-14 10:47:23 -0700

    some update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,15 +6,11 @@
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
+	char string[15]= "_putchar";
+	int i = 0;
+	while(string[i] != '\0'){
+		putchar(string[i]);
+		i++;
+	}
 	return (0);
 }
