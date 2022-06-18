56cfa17443b2c894145601b2ed35c3bcf435098d
jonmes <mesfinjohn@rocketmail.com>
2022-06-18 00:01:34 -0700

    while loop update

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -9,14 +9,10 @@ void _puts(char *str)
 {
 	int count = 0;
 
-	while (count >= 0)
+	while (*(str + count) != '\0')
 	{
-		if (str[count] == '\0')
-		{
-			_putchar('\n');
-			break;
-		}
 		_putchar(str[count]);
 		count++;
-	}
+	};
+	_putchar('\n');
 }
