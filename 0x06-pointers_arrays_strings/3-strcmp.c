b8fee194b9d8ad7cea448634cf3ed9a9283fca83
jonmes <mesfinjohn@rocketmail.com>
2022-06-19 04:59:14 -0700

    alternative method added

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -21,3 +21,20 @@ int _strcmp(char *s1, char *s2)
 	}
 	return (*s1 - *s2);
 }
+/**
+ * ALTERNATIVE SOLUTION
+ *
+ * int _strcmp(char *s1, char *s2)
+ * {
+ *     int i = 0;
+ *     while (s1[i] != '\0' || s2[i] != '0')
+ *     {
+ *         if (s1[i] != s2[i])
+ *         {
+ *             break;
+ *         }
+ *         i++;
+ *     }
+ *     return (s1[i] - s2[i]);
+ * }
+ */
